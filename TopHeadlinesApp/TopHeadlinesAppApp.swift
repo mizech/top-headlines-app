ee62ea85a8c36810197a78b7f6fbaccf54c4a714
mizech <michael.zech@hotmail.de>
2025-01-28 16:14:27 +0100

    Initial Commit

---
+++ TopHeadlinesApp/TopHeadlinesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TopHeadlinesAppApp.swift
+//  TopHeadlinesApp
+//
+//  Created by Michael on 28.01.25.
+//
+
+import SwiftUI
+
+@main
+struct TopHeadlinesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

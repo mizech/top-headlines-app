ab4ae001ae512f4a17047303902922da2fec5672
mizech <michael.zech@hotmail.de>
2025-02-01 10:26:41 +0100

    ArticleDetailsView: Usage of Divider()

--- TopHeadlinesApp/Views/ArticleDetailsView.swift
+++ TopHeadlinesApp/Views/ArticleDetailsView.swift
@@ -5,26 +5,30 @@ struct ArticleDetailsView: View {
     @State var publDate = Date()
     
     var body: some View {
-        VStack(alignment: .leading) {
-            ScrollView {
+        ScrollView {
+            VStack(alignment: .leading, spacing: 10) {
                 Text(article.title ?? "")
                     .font(.title2)
                 Text(article.description ?? "")
+                Divider()
                 Text("Written by: \(article.author ?? "")")
                     .font(.subheadline)
                 Text("Published at: \(publDate.formatted(date: .abbreviated, time: .shortened))")
                     .font(.subheadline)
+                Link("Website Representation", destination: URL(string: article.url ?? "")!)
+                    .font(.subheadline)
+                Divider()
                 Text(article.content ?? "")
                 AsyncImage(url: URL(string: article.urlToImage ?? "")) { image in
                     image.resizable()
-                         .scaledToFit()
-                         .clipShape(RoundedRectangle(cornerRadius: 8))
+                        .scaledToFit()
+                        .clipShape(RoundedRectangle(cornerRadius: 8))
                 } placeholder: {
                     ProgressView()
                 }
             }
+            .padding()
         }
-        .padding()
         .navigationTitle("Headline-details")
         .navigationBarTitleDisplayMode(.inline)
         .onAppear() {

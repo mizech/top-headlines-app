955579757b6d9f873a42d8b44293f18a91c32686
mizech <michael.zech@hotmail.de>
2025-02-01 12:19:50 +0100

    Usage: currTab-variable

--- TopHeadlinesApp/Views/OnboardingView.swift
+++ TopHeadlinesApp/Views/OnboardingView.swift
@@ -2,21 +2,34 @@ import SwiftUI
 
 struct OnboardingView: View {
     @Binding var isOnboardingDone: Bool
+    @State var currTab = 1
     
     var body: some View {
         Text("Top-Headlines App")
             .font(.largeTitle)
         Text("How to use!")
             .font(.title)
-        TabView {
-            VStack(alignment: .leading, spacing: 20) {
+        TabView(selection: $currTab) {
+            VStack(alignment: .leading, spacing: 22) {
                 Text("Use the gear-button on the top to open "
                         + "the settings-screen.")
                 Text("Use the controls to tweak the data, "
                         + "which become displayed.")
+                Button {
+                    currTab = 2
+                } label: {
+                    Text("Next")
+                        .padding()
+                        .frame(height: 40)
+                        .frame(maxWidth: .infinity)
+                        .background(.green)
+                        .foregroundStyle(.white)
+                        .fontWeight(.bold)
+                        .clipShape(RoundedRectangle(cornerRadius: 12))
+                }
                 Spacer()
-            }
-            VStack(spacing: 20) {
+            }.tag(1)
+            VStack(spacing: 22) {
                 Text("Navigation to the details-view of a news-article:"
                         + "Tab on a single headline, within the list.")
                 Button {
@@ -31,8 +44,20 @@ struct OnboardingView: View {
                         .fontWeight(.bold)
                         .clipShape(RoundedRectangle(cornerRadius: 12))
                 }
+                Button {
+                    currTab = 1
+                } label: {
+                    Text("Previous")
+                        .padding()
+                        .frame(height: 40)
+                        .frame(maxWidth: .infinity)
+                        .background(.green)
+                        .foregroundStyle(.white)
+                        .fontWeight(.bold)
+                        .clipShape(RoundedRectangle(cornerRadius: 12))
+                }
                 Spacer()
-            }
+            }.tag(2)
         }
         .padding()
         .tabViewStyle(.page)
